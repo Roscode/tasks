38b03d944bebcef29d8d87d7a085ce351cd3aa13
Darren Roscoe <darrenroscoe@gmail.com>
2018-10-18 22:42:03 -0400

    bootstrapify templates

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,10 @@
-import jQuery from 'jquery';
+import jQuery from "jquery";
 window.jQuery = window.$ = jQuery;
 import "bootstrap";
 // We need to import the CSS so that webpack will load it.
 // The MiniCssExtractPlugin is used to separate it out into
 // its own CSS file.
-import css from "../css/app.css"
+import css from "../css/app.scss";
 
 // webpack automatically bundles all modules in your
 // entry points. Those entry points can be configured
@@ -12,7 +12,7 @@ import css from "../css/app.css"
 //
 // Import dependencies
 //
-import "phoenix_html"
+import "phoenix_html";
 
 // Import local files
 //
